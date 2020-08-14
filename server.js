e136f81a2fc53e2e946c1a7a0a7c888221a9c4ef
Mulubwa17 <mulubwac@gmail.com>
2020-08-14 08:44:20 +0200

    fixing error handling callbacks

--- server.js
+++ server.js
@@ -22,15 +22,24 @@ app.use(
 app.post("/incoming", (req, res) => {
   const date = new Date().toLocaleString();
   const twiml = new MessagingResponse();
-   
+  if (
+    req.body.Body.toLowerCase().trim() != "hi" &&
+    req.body.Body.toLowerCase().trim() != "hello" &&
+    req.body.Body.toLowerCase().trim() != "test" &&
+    req.body.Body.toLowerCase().trim() != "help" &&
+    req.body.Body.toLowerCase().trim() != req.body.body
+  ) {
     request(
       "https://disease.sh/v3/covid-19/countries/" + req.body.Body,
       function (error, response, body) {
         body = JSON.parse(body);
         console.log(body);
-        if (body == req.body ) {
-          body = body;
-       const msg = twiml.message(
+
+        if (body['country'] == "" ) {
+          body['country'] = body;
+        }
+
+        const msg = twiml.message(
           body["country"] +
             "  " +
             // "(" +
@@ -72,14 +81,8 @@ app.post("/incoming", (req, res) => {
         });
         res.end(twiml.toString());
       }
-    
-   else if (
-    req.body.Body.toLowerCase().trim() != "hi" &&
-    req.body.Body.toLowerCase().trim() != "hello" &&
-    req.body.Body.toLowerCase().trim() != "test" &&
-    req.body.Body.toLowerCase().trim() != "help" &&
-    req.body.Body.toLowerCase().trim() === undefined
-  ) {
+    );
+  } else {
     
     var msg = twiml.message(
       `*Hey 👋*
@@ -99,7 +102,7 @@ Seems you mistyped there,` +
     res.end(twiml.toString());
   }
   console.log(req.body);
-      });
+});
 
 app.post("/check", function (req, res) {
   console.log(req.body.Body);
@@ -110,4 +113,4 @@ app.get("/", function (request, response) {
 
 app.listen(process.env.PORT, () => {
   console.log("Server is listening on Port:", process.env.PORT);
-})});
+});
