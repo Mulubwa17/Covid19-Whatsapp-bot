9b262989822068a19dc861bcd603b8df902f3a2a
Mulubwa17 <mulubwac@gmail.com>
2020-08-14 08:48:01 +0200

    fixing error handling callbacks

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.post("/incoming", (req, res) => {
         body = JSON.parse(body);
         console.log(body);
 
-        if (body['country'] == "" ) {
-          body['country'] = body;
+        if (body == "" ) {
+          body = body;
         }
 
         const msg = twiml.message(
